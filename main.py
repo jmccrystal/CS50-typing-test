f790233f5ea9e7b759b2fee8a319ff14ab6f5336
McCrystalJames <30939190+McCrystalJames@users.noreply.github.com>
2021-02-24 22:05:43 -1000

    typing test actually works now

--- main.py
+++ main.py
@@ -1,33 +1,53 @@
+import time
+import math
 import PySimpleGUI as sg
 import other_stuff
 
 sg.theme('Dark')
-sg.SetOptions(font="Georgia 10")
+sg.SetOptions(font="Bahnschrift 15", )
 
-exit = other_stuff.exit
-ok = other_stuff.ok
-words = other_stuff.list_to_string(other_stuff.words)
-
-time_taken = 10
+exit_icon = other_stuff.exit_icon
+ok_icon = other_stuff.ok_icon
+words = other_stuff.list_to_string(other_stuff.type_test)
+time_taken = 0
 
 layout = [
-            [sg.Text("Quick, type!                                          ", key='-TEXT-')],
-            [sg.InputText(key='-INPUT-')],
-            [sg.Button('', image_data=exit, button_color=(sg.theme_background_color(),sg.theme_background_color()), border_width=0, key='-EXIT-'), 
-            sg.Button('', image_data=ok, button_color=(sg.theme_background_color(),sg.theme_background_color()), border_width=0,  key='-OK-')],
-            [sg.Text()]]
+    [sg.Text("Quick, type!", size=(45, None), key='-TEXT-'), sg.Text(time_taken, size=(3, 1), key='-TIMER-')],
+    [sg.Input(key='-INPUT-')],
+    [sg.Button('', image_data=exit_icon, button_color=(sg.theme_background_color(), sg.theme_background_color()),
+               border_width=0, key='-EXIT-'),
+     sg.Button('', image_data=ok_icon, button_color=(sg.theme_background_color(), sg.theme_background_color()),
+               border_width=0, bind_return_key=True, key='-OK-')],
+    [sg.Text(words, size=(50, int(other_stuff.word_amount / 4)))]]
 
 # Create the window
-window = sg.Window('Typing Test', layout, no_titlebar=True, grab_anywhere=True)
+window = sg.Window('Typing Test', layout, no_titlebar=False, grab_anywhere=True)
+test = 1
 
 do_not_reroute_stdout = True
+test_running = False
+test_done = False
 
 # Create an event loop
 while True:
-    event, values = window.read()
-    if event == '-OK-' and window['-INPUT-'].read() == other_stuff.typetest:
-            window['-TEXT-'].update(f"Correct! Your speed was {other_stuff.word_amount / (time_taken / 60)} WPM")
-    # End program if user closes window or
-    # presses the OK button
+    event, values = window.read(timeout=1)
+
+    if event == '-OK-' and values['-INPUT-'] == words:
+        test_running = False
+        test_done = True
+        window['-TEXT-'].update(f"Nice! Your speed was {round(other_stuff.word_amount / (time_taken / 60))} WPM")
+
+    window['-TIMER-'].update(time_taken)
+
+    if len(values['-INPUT-']) > 0 and not test_done:
+        test_running = True
+
+    if not test_running:
+        start_time = time.time() + .5
+
+    if test_running:
+        time_taken = round(time.time() - start_time)
+
+    # End program if user presses the X icon
     if event == '-EXIT-' or event == sg.WIN_CLOSED:
         break
--- other_stuff.py
+++ other_stuff.py
@@ -1,26 +1,32 @@
 import random
 import output
 
-word_amount = 50
+word_amount = 10
 
-exit = output.exit
-ok = output.ok
+exit_icon = output.exit
+ok_icon = output.ok
+
 
 def get_words():
     with open("words.txt", "r") as file:
         return file.read().split(', ')
 
+
 words = get_words()
+
 
 # Generate the random words from the list of words
 def generate_test(num_of_words):
     return random.sample(words, num_of_words)
 
-typetest = generate_test(word_amount)
+
+type_test = generate_test(word_amount)
+
 
 def list_to_string(input_string):
-    output_string = " "  
-    return (output_string.join(input_string)) 
+    output_string = " "
+    return (output_string.join(input_string))
+
 
 if __name__ == "__main__":
-    print(typetest)
+    print(type_test)
