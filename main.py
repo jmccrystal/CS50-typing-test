753b0e38784c801c937b4ac7fde4109ef39c2542
McCrystalJames <30939190+McCrystalJames@users.noreply.github.com>
2021-02-24 23:14:11 -1000

    everything is bigger, minor changes

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import time
-import math
 import PySimpleGUI as sg
 import other_stuff
 
 sg.theme('Dark')
-sg.SetOptions(font="Bahnschrift 15", )
+sg.SetOptions(font="Bahnschrift 25")
 
 exit_icon = other_stuff.exit_icon
 ok_icon = other_stuff.ok_icon
@@ -12,17 +11,16 @@ words = other_stuff.list_to_string(other_stuff.type_test)
 time_taken = 0
 
 layout = [
-    [sg.Text("Quick, type!", size=(45, None), key='-TEXT-'), sg.Text(time_taken, size=(3, 1), key='-TIMER-')],
+    [sg.Text("Quick, type!", size=(33, None), key='-TEXT-'), sg.Text(time_taken, size=(11, 1), justification='right', key='-TIMER-')],
     [sg.Input(key='-INPUT-')],
     [sg.Button('', image_data=exit_icon, button_color=(sg.theme_background_color(), sg.theme_background_color()),
                border_width=0, key='-EXIT-'),
      sg.Button('', image_data=ok_icon, button_color=(sg.theme_background_color(), sg.theme_background_color()),
-               border_width=0, bind_return_key=True, key='-OK-')],
-    [sg.Text(words, size=(50, int(other_stuff.word_amount / 4)))]]
+               border_width=0, pad=(15, None), bind_return_key=True, key='-OK-')],
+    [sg.Text(words, size=(45, int(other_stuff.word_amount / 4)))]]
 
 # Create the window
-window = sg.Window('Typing Test', layout, no_titlebar=False, grab_anywhere=True)
-test = 1
+window = sg.Window('Typing Test', layout, no_titlebar=False, grab_anywhere=False)
 
 do_not_reroute_stdout = True
 test_running = False
@@ -35,9 +33,12 @@ while True:
     if event == '-OK-' and values['-INPUT-'] == words:
         test_running = False
         test_done = True
-        window['-TEXT-'].update(f"Nice! Your speed was {round(other_stuff.word_amount / (time_taken / 60))} WPM")
+        window['-TEXT-'].update(f"Nice! Your speed was {round(other_stuff.word_amount / (time_taken / 60))} WPM.")
 
-    window['-TIMER-'].update(time_taken)
+    elif event == '-OK-' and values['-INPUT-'] != words:
+        window['-TEXT-'].update("Oops! It looks like there is an error!")
+
+    window['-TIMER-'].update(f"{time_taken} seconds")
 
     if len(values['-INPUT-']) > 0 and not test_done:
         test_running = True
